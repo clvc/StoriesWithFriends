63e2e8602eeb3411985cfaefb37f642b887c4134
Colin Cooke <colin11cooke@gmail.com>
2015-02-21 17:38:45 -0500

    Fixing the legs on the couch

--- Stories/app/src/main/java/me/stories/stories/MainActivity.java
+++ Stories/app/src/main/java/me/stories/stories/MainActivity.java
@@ -1,5 +1,6 @@
 package me.stories.stories;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,7 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.couchbase.lite.*;
+import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.util.Log;
+
+import org.codehaus.jackson.map.deser.SettableBeanProperty;
+
+import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,6 +27,8 @@ public class MainActivity extends ActionBarActivity {
     private Button button;
     private TextView storyText;
     private EditText newText;
+    private Manager manager;
+    private Database database;
     private String prevNewString;
 
 
@@ -43,6 +54,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createManger();
+        initDatabase();
+
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         button = (Button) findViewById(R.id.submitButton);
@@ -59,11 +73,41 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    private void initDatabase() {
+        // create a name for the database and make sure the name is legal
+        String dbname = "hello";
+        if (!Manager.isValidDatabaseName(dbname)) {
+            return;
+        }
+// create a new database
 
+        try {
+            database = manager.getDatabase(dbname);
+        } catch (CouchbaseLiteException e) {
+            Toast.makeText(this,"DATABASE FUCKING FAILED", Toast.LENGTH_SHORT );
+            return;
+        }
+
+    }
+
+
+    private void createManger(){
+
+        try {
+            manager = new Manager(new AndroidContext(this), Manager.DEFAULT_OPTIONS);
+            Log.d("TAGGGGGG", "Manager created");
+        } catch (IOException e) {
+            Log.e("TAGGGGGG", "Cannot create manager object");
+            e.printStackTrace();
+            Toast.makeText(this,"MANAGER FUCKING FAILED", Toast.LENGTH_SHORT );
+        }
+
+    }
 
     private void submitText() {
         String currentText = storyText.getText().toString();
         String toAdd = newText.getText().toString().split(" ")[0];
+        newText.setText("");
 
         toAdd = toAdd.split(" ")[0];
         currentText += " " + toAdd;
